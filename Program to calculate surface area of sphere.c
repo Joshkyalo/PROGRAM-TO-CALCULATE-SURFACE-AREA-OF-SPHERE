66619430d6535d658cf5dad11409dc0d240037a1
muthixyz <99032265+muthixyz@users.noreply.github.com>
2022-03-26 13:46:40 +0300

    Add files via upload
    HILARIOUS

---
+++ Program to calculate surface area of sphere.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* PROGRAM TO CALCULATE SURFACE AREA OF SPHERE*/
+
+int main(int argc, char *argv[])
+ { 
+
+float radius;
+float surface_area;
+printf("Enter radius of the sphere:\n");
+scanf("%f",&radius);
+surface_area=4*(22/7)*radius*radius;
+printf("surface area of sphere is :%.3f",surface_area);
+return 0;
+}
+
